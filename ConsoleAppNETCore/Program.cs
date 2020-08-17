a3e4951143cc7f0f0e798c03880dfc1b6ea3cb60
TanyaPivovarchick <tatsiana.pivovarchick@gmail.com>
2020-08-17 07:41:07 +0300

    Generic host

--- ConsoleAppNETCore/Program.cs
+++ ConsoleAppNETCore/Program.cs
@@ -1,13 +1,34 @@
-﻿using SharedLibraryNETStandard;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SharedLibraryNETStandard;
 using System;
+using System.IO;
+using System.Threading.Tasks;
 
-namespace ConsoleApp
+namespace ConsoleAppNETCore
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine(TestClass.GetCurrentDate());
+
+            await CreateHostBuilder(args).Build().RunAsync();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureHostConfiguration(configHost =>
+                {
+                    configHost.SetBasePath(Directory.GetCurrentDirectory());
+                    configHost.AddJsonFile("hostsettings.json", optional: true);
+                    configHost.AddEnvironmentVariables(prefix: "PREFIX_");
+                    configHost.AddCommandLine(args);
+                })
+                .ConfigureServices(services =>
+                {
+                    services.AddSingleton<IHostedService, TestService>();
+                });
     }
 }
---
+++ ConsoleAppNETCore/TestService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleAppNETCore
+{
+    public class TestService : IHostedService
+    {
+        private ILogger Logger { get; }
+
+        public TestService(ILogger<TestService> logger)
+        {
+            Logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Logger.LogInformation("Starting test service");
+            Logger.LogInformation(ReturnNewString());
+
+            return Task.CompletedTask;
+        }
+
+        public string ReturnNewString()
+        {
+            return "Some test string";
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Logger.LogInformation("Stopping test service");
+
+            return Task.CompletedTask;
+        }
+    }
+}
